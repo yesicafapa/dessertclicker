d52e1d735a51d29b2a1ecaede096e088ea28486b
yesicafapa <aciseyyes@gmail.com>
2025-11-21 22:49:45 +0700

    Initial commit

---
+++ app/src/main/java/com/example/dessertclicker/data/Datasource.kt
@@ -0,0 +1,22 @@
+package com.example.dessertclicker.data
+
+import com.example.dessertclicker.R
+import com.example.dessertclicker.model.Dessert
+
+object Datasource {
+    val dessertList = listOf(
+        Dessert(R.drawable.cupcake, 5, 0),
+        Dessert(R.drawable.donut, 10, 5),
+        Dessert(R.drawable.eclair, 15, 10),
+        Dessert(R.drawable.froyo, 30, 15),
+        Dessert(R.drawable.gingerbread, 50, 20),
+        Dessert(R.drawable.honeycomb, 100, 22),
+        Dessert(R.drawable.icecreamsandwich, 500, 24),
+        Dessert(R.drawable.jellybean, 1000,  28),
+        Dessert(R.drawable.kitkat, 2000, 30),
+        Dessert(R.drawable.lollipop, 3000, 32),
+        Dessert(R.drawable.marshmallow, 4000, 35),
+        Dessert(R.drawable.nougat, 5000, 38),
+        Dessert(R.drawable.oreo, 6000, 40)
+    )
+}
